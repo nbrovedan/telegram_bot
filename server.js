32a02f9d0ca2ee5a132478d054eb9aee6bce38ec
Glitch (hello-express) <none>
2020-02-07 19:30:49 +0000

    😜🏐 Checkpoint ./server.js:11157041/1446

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-const axios = require('axios')
-const {
-    Telegram
-} = require('telegraf')
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+const axios = require("axios");
+const { Telegram } = require("telegraf");
 
-const tg = new Telegram(process.env.BOT_TOKEN)
+const tg = new Telegram(process.env.BOT_TOKEN);
 
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json()); // for parsing application/json
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-)
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/random-jokes', function(req, res) {
-    axios.get('https://api.chucknorris.io/jokes/random').then(res => {
-        const txt = res.data.value
-        tg.sendMessage(process.env.GROUP_ID, txt)
-    })
-    res.send('Joke is delivered')
-})
+app.get("/random-jokes", function(req, res) {
+  axios.get("https://api.chucknorris.io/jokes/random").then(res => {
+    const txt = res.data.value;
+    tg.sendMessage(process.env.CHANNEL_ID, txt);
+  });
+  res.send("Joke is delivered");
+});
 
 // Finally, start our server
 app.listen(3000, function() {
-    console.log('Telegram app listening on port 3000!')
-})
+  console.log("Telegram app listening on port 3000!");
+});
