3edb0cdfc4fca0d228cf0c0c3b56e57317c64874
Glitch (lembreteclaro) <none>
2020-02-07 20:00:45 +0000

    🏇🍚 Checkpoint ./server.js:11157041/1363 ./package.json:11157041/34

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var bodyParser = require("body-parser");
 const axios = require("axios");
 const { Telegram } = require("telegraf");
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<password>@cluster0-niqji.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(collection);
+  client.close();
+});
+
+
+
+
 const tg = new Telegram(process.env.BOT_TOKEN);
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -14,9 +26,9 @@ app.use(
   })
 );
 
-app.get("/", function(req, res) {
+app.get("/lembrar", function(req, res) {
   tg.sendMessage(process.env.CHANNEL_ID, "Lembrete qualquer");
-  res.send("Joke is delivered");
+  res.send("Enviado...");
 });
 
 // Finally, start our server
