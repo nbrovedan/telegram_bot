0270b334656e63cf2651da4a1f3c2671e3375541
Naidion <naidion.brovedan@terceiros.calcard.com.br>
2020-02-14 11:48:50 -0300

    Ajustado erros

--- libs/db.js
+++ libs/db.js
@@ -2,8 +2,6 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:<password>@cluster0-niqji.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-
-
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   //console.log(collection);
